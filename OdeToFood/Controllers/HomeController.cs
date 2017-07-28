990498dc3df55395f943c4bfececc0676c125c5f
Guilherme Silva dos Santos <guisant0s@outlook.com>
2017-07-28 18:20:13 -0300

    Novas rotas.

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -7,6 +7,14 @@ namespace OdeToFood.Controllers
     {
         public ActionResult Index()
         {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"];
+
+            var message = string.Format("{0}::{1} {2}", controller, action, id);
+
+            ViewBag.Message = message;
+
             return View();
         }
 
