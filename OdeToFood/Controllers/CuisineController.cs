0e1b7546ae2aeec4a72c59125735d207f71c2266
Guilherme Silva dos Santos <guisant0s@outlook.com>
2017-08-02 18:22:41 -0300

    Finalizado o modulo de controladores.

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -1,18 +1,31 @@
-﻿using System.Web.Mvc;
+﻿using OdeToFood.Filters;
+using System.Web.Mvc;
 
 namespace OdeToFood.Controllers
 {
     public class CuisineController : Controller
     {
         // GET: Cuisine
+        //[Authorize]
+        [Log]
         public ActionResult Search(string name = "french")
         {
+
+            //throw new Exception("teste");
+
             var message = Server.HtmlEncode(name);
-            //return Content(message);
+            return Content(message);
+
             //return RedirectToAction("Index", "Home", new { name = name });
             //return RedirectToRoute("Default", new { controller = "Home", action = "About" });
             //return File(Server.MapPath("~/Content/site.css"), "text/css");
-            return Json(new { Message = message, Name = "Guilherme" }, JsonRequestBehavior.AllowGet);
+            //return Json(new { Message = message, Name = "Guilherme" }, JsonRequestBehavior.AllowGet);
         }
+
+        //[HttpGet]
+        //public ActionResult Search()
+        //{
+        //    return Content("Search!");
+        //}
     }
 }
